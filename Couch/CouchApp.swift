93e7d9e0f9decdbe52670329cf9015b206da11b0
MarinaMamdouh <marina.mam.riad@gmail.com>
2022-09-05 16:35:06 +0200

    Initial Commit

---
+++ Couch/CouchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CouchApp.swift
+//  Couch
+//
+//  Created by Marina on 05/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct CouchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
