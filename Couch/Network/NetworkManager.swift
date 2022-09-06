a7463afc677574ee39380b8ac05363f3f9b851d4
MarinaMamdouh <marina.mam.riad@gmail.com>
2022-09-06 22:46:33 +0200

    Add NetworkManager

---
+++ Couch/Network/NetworkManager.swift
@@ -0,0 +1,34 @@
+//
+//  NetworkManager.swift
+//  Couch
+//
+//  Created by Marina on 06/09/2022.
+//
+
+import Foundation
+import Combine
+
+class NetworkManager{
+    static func performRequest(_ request:URLRequest) -> AnyPublisher<Data, Error>{
+         return URLSession.shared.dataTaskPublisher(for: request)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap { (output) -> Data in
+                guard let response = output.response as? HTTPURLResponse,
+                      response.statusCode >= 200 && response.statusCode < 300 else {
+                          throw URLError(.badServerResponse)
+                }
+                return output.data
+            }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+    
+    static func handleCompletion(_ completion: Subscribers.Completion<Error>){
+        switch completion{
+        case .finished:
+            break
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+}
