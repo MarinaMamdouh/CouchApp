aa9966215cae9c83ff5d2222a38946cb0b278268
MarinaMamdouh <marina.mam.riad@gmail.com>
2022-09-08 19:47:33 +0200

    add comments to the NetworkManager for more clearer info

--- Couch/Network/NetworkManager.swift
+++ Couch/Network/NetworkManager.swift
@@ -9,6 +9,9 @@ import Foundation
 import Combine
 
 class NetworkManager{
+    
+    // perform a given request by Creating a publisher that publishes a retrieved data when task is completed
+    // return that created publisher
     static func performRequest(_ request:URLRequest) -> AnyPublisher<Data, Error>{
          return URLSession.shared.dataTaskPublisher(for: request)
             .subscribe(on: DispatchQueue.global(qos: .default))
@@ -17,6 +20,9 @@ class NetworkManager{
             .eraseToAnyPublisher()
     }
     
+    // handle the response output from a given request
+    // checks if response is successful or not
+    // return the output data
     private static func handleResponse(from request: URLRequest ,output: URLSession.DataTaskPublisher.Output) throws -> Data {
         guard let response = output.response as? HTTPURLResponse,
               response.statusCode >= 200 && response.statusCode < 300 else {
@@ -25,6 +31,8 @@ class NetworkManager{
         return output.data
     }
     
+    // handle the completion of the task
+    // check if it is successfully finished or has failed
     static func handleCompletion(_ completion: Subscribers.Completion<Error>){
         switch completion{
         case .finished:
